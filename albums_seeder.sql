bd4a718b67c4c5c41bcef6c6b1705078c3ac2494
Tyler Warren <warrejt@yahoo.com>
2016-07-25 16:15:59 -0500

    adding albums to the database

---
+++ albums_seeder.sql
@@ -0,0 +1,17 @@
+use codeup_test_db;
+
+insert into albums (
+	artist,
+	record,
+	release_date, 
+	genre, 
+	sales
+)
+
+values 
+	('Michael Jackson', 'Thriller', 1982, 'Pop rock R&B', '45.4'),
+	('Eagles', 'Their Greatest Hits (1971–1975)', 1976,	'Rock soft rock',	
+'32.2'),
+	('Nirvana', 'Nevermind', 1991, 'Grunge', '16.7'),
+	('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', 1967,	'Rock',	
+'13.1');
