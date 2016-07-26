af639034fa93db6e782a7eee04b17db519b046fd
Tyler Warren <warrejt@yahoo.com>
2016-07-26 13:57:51 -0500

    correcting update exercise

--- update_exercises.sql
+++ update_exercises.sql
@@ -3,7 +3,7 @@ SELECT * from albums;
 UPDATE albums SET sales = sales*10;
 -- 2ii
 SELECT record from albums where release_date < 1980;
-UPDATE albums SET realeas_date = 1800 where release_date < 1980;
+UPDATE albums SET release_date = 1800 where release_date < 1980;
 -- 2iii
 SELECT record from albums where artist = 'Michael Jackson';
 UPDATE albums SET artist = 'Peter Jackson' where artist = 'Michael Jackson';
