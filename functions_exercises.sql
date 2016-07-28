9b0d2b8d830e2ae253c918e3683831c45dbbb6ec
Tyler Warren <warrejt@yahoo.com>
2016-07-28 10:26:15 -0500

    exercise completed

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -8,4 +8,4 @@ SELECT CONCAT(last_name, ' ', first_name) FROM employees WHERE last_name LIKE ('
 SELECT last_name, first_name, DATEDIFF(curdate(), hire_date) as time_period FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25' ORDER BY time_period DESC, last_name, first_name;
 
 --5)
-SELECT last_name, first_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' ORDER BY last_name, first_name;
+SELECT last_name, first_name, count(*) FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name, first_name ORDER BY last_name, first_name;
